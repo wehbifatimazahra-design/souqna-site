d66e468fea2d97145b155ecb2285dba15a129434
wehbifatimazahra-design <wehbifatimazahra@gmail.com>
2026-01-06 18:47:15 +0100

    Add files via upload
    رفع ملفات موقع “سوقنا” بالكامل

---
+++ script.js
@@ -0,0 +1,9 @@
+function afficherMessage() {
+    alert("شكراً لتسوقك معنا في سوقنا!");
+}
+
+function showFormMessage(event) {
+    event.preventDefault();
+    alert("تم إرسال النموذج بنجاح! سنتواصل معك قريباً.");
+}
+
